d2ff568623cf21be37caf42776ecbfddaa3e53b4
Zax37 <c37zax@gmail.com>
2018-03-13 12:04:49 +0100

    Publishing fatJar to local repository.

---
+++ src/main/java/pl/zax37/fatjar/Library.java
@@ -0,0 +1,9 @@
+package pl.zax37.fatjar;
+
+import org.apache.commons.lang.StringUtils;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return StringUtils.isNotEmpty("String");
+    }
+}
